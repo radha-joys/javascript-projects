dde98ae20ad2f4d2b52f01fb1ad17996de066d81
unknown <radhasrivepuri@gmail.com>
2024-02-14 16:17:51 -0600

    Class 10 Exercises: Exceptions

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,13 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result = 0;
+    try{
+      result = lab.runLab(3);
+    }
+    catch(e){
+      result = e;
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -18,7 +24,29 @@ let studentLabs = [
     runLab: function (num) {
         return num * num;
     }
+  },
+];
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
   }
 ];
 
 gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
