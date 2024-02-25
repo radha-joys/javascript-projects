a53f71939c6e53346145486bf94bd951aefda565
unknown <radhasrivepuri@gmail.com>
2024-02-25 14:03:10 -0600

    Class 14 Exercises: DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,30 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", () => {
+        paragraph.innerText = "Houston, we have liftoff!";
+        console.log(paragraph);
+    })
+
+    missionAbort.addEventListener("mouseover", () => {
+        missionAbort.style.backgroundColor = "red";
+        missionAbort.style.borderColor = "red";
+        missionAbort.style.color = "white";
+    })
+
+    missionAbort.addEventListener("mouseout", () => {
+        missionAbort.style.backgroundColor = "";
+        missionAbort.style.borderColor = "";
+        missionAbort.style.color = "";
+    })
+
+    missionAbort.addEventListener("click",  () => {
+        console.log('clicked abort!')
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if(response){
+            window.alert("Mission aborted! Space shuttle returning home");
+        }
+    })
 }
 
 window.addEventListener("load", init);
