b7bf63d2e0614e4cd70a123a2bd101dea9974c9b
unknown <radhasrivepuri@gmail.com>
2024-03-17 10:22:19 -0500

    Class 3 Exercises: Strings & Arrays

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,11 +3,12 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
-
+let changedLang = language.slice(0,1) + language.slice(4, 5);
+console.log(changedLang);
 //2. Without using slice(), use method chaining to accomplish the same thing.
-
+changedLang = language.charAt(0).concat(language.charAt(4));
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for ${language} is ${changedLang}`)
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
 //Part Three section Two
@@ -15,3 +16,5 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+notTitleCase = notTitleCase.split(" ").map((ele) => ele = ele.slice(0,1).toUpperCase() + ele.slice(1));
+console.log(notTitleCase);
--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -34,3 +34,4 @@ console.log(dna.slice(16, 19));
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 console.log(`The DNA strand is ${dnaTwo.length} characters long`)
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+console.log(`Taco Cat`)
