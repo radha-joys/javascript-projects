2c7f795de612e60d94c5a7d4b3b84116070d8b4c
unknown <radhasrivepuri@gmail.com>
2024-03-18 13:52:55 -0500

    Class 5 Studio: Functions Are at Your Beck and Call

--- functions/studio/studio-functions.js
+++ functions/studio/studio-functions.js
@@ -3,32 +3,47 @@
 // Part One: Reverse Characters
 
 // 1. Define the function as reverseCharacters. Give it one parameter, which will be the string to reverse.
-// 2. Within the function, split the string into an array, then reverse the array.
-// 3. Use join to create the reversed string and return that string from the function.
+let reverseCharacters = (str) => {
+    // 2. Within the function, split the string into an array, then reverse the array.
+    // 3. Use join to create the reversed string and return that string from the function.
+    // Part Two: Reverse Digits
+    // 1. Add an if statement to reverseCharacters to check the typeof the parameter.
+    // 2. If typeof is ‘string’, return the reversed string as before.
+    // 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
+    // 4. Return the reversed number.
+    // 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
+    if(typeof(str) === 'string'){
+        return str.split('').reverse().join('')
+    }
+    else if(typeof(str) === 'number'){
+        return String(str).split('').reverse().join('')
+    }
+    
+}
 // 4. Below the function, define and initialize a variable to hold a string.
+let str = 123;
 // 5. Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
+console.log(reverseCharacters(str));
 // 6. Optional: Use method chaining to reduce the lines of code within the function.
 
-// Part Two: Reverse Digits
-
-// 1. Add an if statement to reverseCharacters to check the typeof the parameter.
-// 2. If typeof is ‘string’, return the reversed string as before.
-// 3. If typeof is ’number’, convert the parameter to a string, reverse the characters, then convert it back into a number.
-// 4. Return the reversed number.
-// 5. Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
-
 // Part Three: Complete Reversal
-
-// 1. Define and initialize an empty array.
-// 2. Loop through the old array.
-// 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
-// 4. Add the reversed string (or number) to the array defined in part ‘a’.
-// 5. Return the final, reversed array.
-// 6. Be sure to print the results from each test case in order to verify your code.
-
+let completeReversal = (initialArray) => {
+    // 1. Define and initialize an empty array.
+    let arr = [];
+    // 2. Loop through the old array.
+    // 3. For each element in the old array, call reverseCharacters to flip the characters or digits.
+    // 4. Add the reversed string (or number) to the array defined in part ‘a’.
+    // 5. Return the final, reversed array.
+    // 6. Be sure to print the results from each test case in order to verify your code.
+    initialArray.map((ele) => arr.push(reverseCharacters(ele)))
+    return arr;
+}
 let arrayTest1 = ['apple', 'potato', 'Capitalized Words'];
 let arrayTest2 = [123, 8897, 42, 1168, 8675309];
 let arrayTest3 = ['hello', 'world', 123, 'orange'];
+console.log(completeReversal(arrayTest1));
+console.log(completeReversal(arrayTest2));
+console.log(completeReversal(arrayTest3));
 
 // Bonus Missions
 
